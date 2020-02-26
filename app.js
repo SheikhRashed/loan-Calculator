9ec35585cd609037d474cd1795f1585a2078dcaa
Sheikh Rashed <mdrashed1310@gmail.com>
2020-02-27 04:47:07 +0600

    Done Loan Calculator ;

---
+++ app.js
@@ -0,0 +1,77 @@
+// Get loan form
+document.getElementById('loan-form').addEventListener('submit', (e) =>{
+  // Hide Results
+  document.getElementById('results').style.display = 'none';
+  // Show loader
+  document.getElementById('loader').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+  e.preventDefault();
+});
+
+// Add event listeners function 
+function calculateResults(){
+  // define vaiables
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment =  document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+
+  // Compute Monthly Payment
+  const x  = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal*x*calculatedInterest) / (x-1);
+
+  if(isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    // Show Results
+  document.getElementById('results').style.display = 'block';
+  // Hide loader
+  document.getElementById('loader').style.display = 'none';
+  } else {
+   showError('Please Check Your Numbers');
+  }
+  // clear Input field after showing result
+  amount.value = '';
+  interest.value = '';
+  years.value = '';
+}
+
+// Define show Error Function
+function showError(error) {
+
+      // Hide Results
+      document.getElementById('results').style.display = 'none';
+      // Hide loader
+      document.getElementById('loader').style.display = 'none';
+
+ const errorDiv = document.createElement('div');
+
+   // Get Elements 
+   const card = document.querySelector('.card');
+   const heading = document.querySelector('.heading');
+//  add class
+  errorDiv.className = 'alert alert-danger';
+
+  // create text node
+  errorDiv.appendChild(document.createTextNode(error));
+  // insertBefore
+
+// Insert before heading
+  card.insertBefore(errorDiv, heading);
+
+  // clear after 3s
+  setTimeout(() =>{
+    document.querySelector('.alert').remove();
+  }, 3000);
+  
+}
